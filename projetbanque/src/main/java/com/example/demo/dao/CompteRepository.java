d4bd07a43df680e3601a0c90d83d555b940e1c57
moad <moad95@live.fr>
2019-12-22 13:05:09 +0100

    new branch

--- projetbanque/src/main/java/com/example/demo/dao/CompteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.*;
-
-public interface CompteRepository extends JpaRepository<Compte,String> {
-
-}
