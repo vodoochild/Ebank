d8d338961018bf4f31c72c1b44f0c1954553001a
moad <moad95@live.fr>
2019-12-22 12:45:32 +0100

    delet python directory

--- operator_python/operator/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app2.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
--- operator_python/operator/myApp/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-from .app import app
-from .models import model
-# Connect sqlalchemy to app
-model.db.init_app(app)
